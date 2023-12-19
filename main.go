86b00aab78d811a6d1ddb32d61f8101efe2e9a44
teurboonyanu <thanapat.eurboonyanun@agoda.com>
2023-12-19 13:25:13 +0700

    initiate mqtt client draft

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +19,34 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
 func main() {
+	var broker = ""
+	var port = 1883
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID("go-mqtt-client")
+	opts.SetUsername("")
+	opts.SetPassword("")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
 
@@ -28,4 +57,6 @@ func main() {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+
+	client.Disconnect(250)
 }
