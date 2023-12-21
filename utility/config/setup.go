1d66432258b26f613a122d639255eef5815aa8b0
Kittiphum Kraisit <acp49253@gmail.com>
2023-12-21 10:15:14 +0700

    add echo framework

---
+++ utility/config/setup.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	server_port int
+	MQTT_port   int
+	MQTT_server string
+}
+
+var GlobalConfig *Config
+
+func Setup() {
+	viper.SetConfigFile(".env")
+
+	GlobalConfig = &Config{
+		server_port: viper.GetInt("SERVER_PORT"),
+		MQTT_port:   viper.GetInt("MQTT_PORT"),
+		MQTT_server: viper.GetString("MQTT_SERVER"),
+	}
+
+}
