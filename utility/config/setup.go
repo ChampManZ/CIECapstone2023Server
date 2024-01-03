e36da01a585a6fe7134dc65d318685380c26d9f1
Kittiphum Kraisit <acp49253@gmail.com>
2024-01-03 15:11:48 +0700

    fix MQTT & add new env

--- utility/config/setup.go
+++ utility/config/setup.go
@@ -5,9 +5,11 @@ import (
 )
 
 type Config struct {
-	server_port int
-	MQTT_port   int
-	MQTT_server string
+	server_port   int
+	MQTT_port     int
+	MQTT_server   string
+	MQTT_username string
+	MQTT_password string
 }
 
 var GlobalConfig *Config
@@ -17,9 +19,11 @@ func Setup() {
 	viper.ReadInConfig()
 
 	GlobalConfig = &Config{
-		server_port: viper.GetInt("SERVER_PORT"),
-		MQTT_port:   viper.GetInt("MQTT_PORT"),
-		MQTT_server: viper.GetString("MQTT_SERVER"),
+		server_port:   viper.GetInt("SERVER_PORT"),
+		MQTT_port:     viper.GetInt("MQTT_PORT"),
+		MQTT_server:   viper.GetString("MQTT_SERVER"),
+		MQTT_username: viper.GetString("MQTT_USERNAME"),
+		MQTT_password: viper.GetString("MQTT_PASSWORD"),
 	}
 
 }
